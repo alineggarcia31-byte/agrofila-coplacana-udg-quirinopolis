4632e86e3a774e491d532d86efee77be876afe12
alineggarcia31-byte <aline.ggarcia31@gmail.com>
2026-09-16 08:30:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 // NUNCA coloque a service_role key neste arquivo.
 
 window.AGROFILA_CONFIG = {
-  SUPABASE_URL: "https://suyebqrcaqfslfowtli.supabase.co",
+  SUPABASE_URL: "https://suyebqrcaqfslfowtili.supabase.co",
   SUPABASE_KEY: "sb_publishable_QxwXJ--E-y9ssK09XRYtlA_aOZPsmqh"
 };
